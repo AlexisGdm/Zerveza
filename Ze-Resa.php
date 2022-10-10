8e39ab01bdf050ac7c31eae6332d7d10922ff445
AlexisGdm <alexis.godmer@gmx.fr>
2022-10-10 14:00:51 +0200

    14h

--- Ze-Resa.php
+++ Ze-Resa.php
@@ -12,41 +12,42 @@ include('ZE-header.php');
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"])) {
-        $nameErr = "Merci d'entrer votre nom ou votre prénom";
-    } else {
-        $name = test_input($_POST["name"]);
-        // check if name only contains letters and whitespace
-        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-            $nameErr = "Seules les lettres ou espaces sont autorisés";
-        }
-    }
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     if (empty($_POST["name"])) {
+//         $nameErr = "Merci d'entrer votre nom ou votre prénom";
+//     } else {
+//         $name = test_input($_POST["name"]);
+//         // check if name only contains letters and whitespace
+//         if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+//             $nameErr = "Seules les lettres ou espaces sont autorisés";
+//         }
+//     }
 
-    if (empty($_POST["email"])) {
-        $emailErr = "Merci d'entrer votre email";
-    } else {
-        $email = test_input($_POST["email"]);
-        // check if e-mail address is well-formed
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "Format de l'email invalide";
-        }
-    }
+//     if (empty($_POST["email"])) {
+//         $emailErr = "Merci d'entrer votre email";
+//     } else {
+//         $email = test_input($_POST["email"]);
+//         // check if e-mail address is well-formed
+//         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//             $emailErr = "Format de l'email invalide";
+//         }
+//     }
 
-    if (empty($_POST["comment"])) {
-        $comment = "";
-    } else {
-        $comment = test_input($_POST["comment"]);
-    }
-}
+//     if (empty($_POST["comment"])) {
+//         $comment = "";
+//     } else {
+//         $comment = test_input($_POST["comment"]);
+//     }
+// }
 
-function test_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+// function test_input($data)
+// {
+//     $data = trim($data);
+//     $data = stripslashes($data);
+//     $data = htmlspecialchars($data);
+//     return $data;
+// }
+// 
 ?>
 
 <main>
@@ -56,13 +57,13 @@ function test_input($data)
             <div class="formulaire">
                 <form method="post" action="ZE-Resa2.php">
                     <div id="Resa">
-                        <input type="text" name="Nom" id="Nom" placeholder="Nom" size="40" required />
+                        <input type="text" name="name" id="Nom" placeholder="Nom" size="40" required value="<?php echo $name; ?>/>
                         <br>
-                        <input type="text" name="Prenom" id="Prenom" placeholder="Prénom" size="40" required />
+                        <input type=" text" name="name" id="Prenom" placeholder="Prénom" size="40" required />
                         <br>
                         <input type="tel" name="Telephone" id="Telephone" placeholder="00 00 00 00 00" size="14" required />
                         <br>
-                        <input type="email" name="Email" id="Email" placeholder="exemple@email.com" size="40" required />
+                        <input type="email" name="email" id="Email" placeholder="exemple@email.com" size="40" required />
                         <br>
                         <input type="datetime-local" name="Date_Resa" id="Date_Resa" required />
                         <br>
