dab0b3c1830dacf4871843e781846d97a1286e51
AlexisGdm <alexis.godmer@gmx.fr>
2022-11-04 14:00:23 +0100

    04/11 14h

--- admin-add/index.php
+++ admin-add/index.php
@@ -4,6 +4,6 @@ require_once("../sql/connectdb.php");
 include("../head/ZE-controller-head.php");
 require('../header-admin/ZE-controller-header-admin.php');
 require("../admin-auth/ZE-controller-admin-auth.php");
+require('templates/ZE-admin-add.php');
 require("src/ZE-model-admin-add.php");
-require('./templates/ZE-admin-add.php');
 require('../footer/ZE-controller-footer.php');
--- admin-add/templates/ZE-admin-add.php
+++ admin-add/templates/ZE-admin-add.php
@@ -1,15 +1,22 @@
 <?php
 $login = new Login;
 $login->authorize();
+$login = new Login;
+$login->authorize();
+$db = connectDb();
+$sqlRequest = 'SELECT DISTINCT TYPE_beer FROM beer ';
+$sqlResponse = $db->prepare($sqlRequest);
+$sqlResponse->execute();
+$results = $sqlResponse->fetchAll(PDO::FETCH_OBJ);
 ?>
 <div class="add-beer">
     <h4>Saisie d'une nouvelle bière !</h4>
 </div>
 <div class="add-beer">
-    <form method="get" action="//Zerveza/admin-add-2">
+    <form method="get" action="//Zerveza/admin-add">
         <div class="mb-3">
             <label for="title-beer-second" class="form-label">Nom :</label>
-            <input type="text" class="form-control" id="NAME_beer" name="NAME_beer" required>
+            <input type="text" class="form-control" name="NAME_beer" required>
         </div>
         <div class="mb-3">
             <label for="genreselect" class="form-label">Selectionne un type</label>
@@ -25,16 +32,36 @@ $login->authorize();
         </div>
         <div class="mb-3">
             <label for="refimg" class="form-label">Photo de la bière :</label>
-            <input type="text" class="form-control" id="PHOTO_beer" name="PHOTO_beer" required>
+            <input type="file" class="form-control" name="photo-beer" required>
         </div>
-
         <div class="mb-3">
             <label for="resume" class="form-label">Résumé :</label>
-            <textarea type="text" name="Message" id="Message" rows="6" cols=35 required></textarea>
+            <textarea type="text" name="txt-beer" rows="6" cols=35 required></textarea>
         </div>
-
         <div class="mb-3">
-            <button type="submit" class="btn btn-primary">CREER</button>
+            <button type="submit" class="btn btn-primary" name="submit-admin-add">CREER</button>
         </div>
     </form>
 </div>
+
+<?php
+if(isset($_GET['submit-admin-add'])) {
+    $img_name  = $_FILES['photo-beer']['name'];
+    $tmp_img_name = $_FILES['photo-beer']['tmp_name'];
+    $folder = '//Zerveza/assets/images/' . $img_name;
+    move_uploaded_file($tmp_img_name ,$folder.$img_name);
+    $NAME_beer = $_GET['NAME_beer'];
+    $TYPE_beer = $_GET['choice'];
+    $DESC_beer = $_GET['txt-beer'];
+    $PHOTO_beer = $_GET["photo-beer"];
+    $db = connectDb();
+    $sqlRequest2 = "INSERT INTO beer (NAME_beer, TYPE_beer, DESC_beer, PHOTO_beer) VALUES (:NAME_beer, :choice, :txt-beer, :photo-beer)";
+    $sql = $db->prepare($sqlRequest2);
+    $sql->bindParam('NAME_beer', $NAME_beer, PDO::PARAM_STR);
+    $sql->bindParam('TYPE_beer', $TYPE_beer, PDO::PARAM_STR);
+    $sql->bindParam('DESC_beer', $DESC_beer, PDO::PARAM_STR);
+    $sql->bindParam('PHOTO_beer', $PHOTO_beer, PDO::PARAM_STR);
+    $sql->execute();
+    $db = disconnectDb();
+    echo "Bière $NAME_beer ajoutée avec succès";
+}
